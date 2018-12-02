164066e68d7c5a99686e21143579107f35071579
Evgenia <evgeniagaletskaya@gmail.com>
2018-12-02 14:15:22 +0300

    Create second screen. Add WeatherViewController

---
+++ WeatherApp/WeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Evgenia Galetskaya on 12/2/18.
+//  Copyright © 2018 Evgenia Galetskaya. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
